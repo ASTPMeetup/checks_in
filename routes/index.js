1978835c4c194f33ef6306192b80b31c0214185e
PieroAPretto <pretto417@gmail.com>
2016-05-10 16:47:33 -0700

    Finished attendance app

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,26 @@
 var express = require('express');
 var router = express.Router();
 
+var students = {};
+
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', {
+    title: 'Student Check Ins!',
+    students: students
+  });
+});
+
+router.post('/students', function(req, res, next) {
+  var student = req.body.student;
+  if (!(student in students)){
+    students[student] = 1;
+  }
+  else {
+    students[student]++;
+  }
+  res.redirect('/');
 });
 
 module.exports = router;
